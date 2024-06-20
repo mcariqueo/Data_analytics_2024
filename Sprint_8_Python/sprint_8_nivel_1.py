5f9476f3c62800eba56d06ada3020a6ee3fa807a
Marcial Cariqueo A <marcial.cariqueo@gmail.com>
2024-06-20 13:59:17 +0200

    update

---
+++ Sprint_8_Python/sprint_8_nivel_1.py
@@ -0,0 +1,12 @@
+Installing Connector/Python with pip
+Using pip is the recommended way to install Connector/Python and it functions on all standard systems, including Windows, and installing the Python language also installs pip.
+
+
+# Installation
+pip install mysql-connector-python
+
+# Upgrade
+pip install mysql-connector-python --upgrade
+
+# Optional, installs the X DevAPI interface
+pip install mysqlx-connector-python
